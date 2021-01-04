baf434cf9a646cd2bc08184d71187f7f23aef032
David Suh <davidisuh93@gmail.com>
2021-01-04 09:26:53 -0500

    added removeClass

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -27,9 +27,10 @@ class DOMNodeCollection {
         else if(typeof arg === 'string'){
             // let NodeList = document.querySelectorAll(arg);
             // make a node list with blank HTMLElements
-            let NodeList = document.createElement(arg).innerHTML = "Hello World";
-            NodeList = Array.from(NodeList);
-            domNode = new DOMNodeCollection(NodeList);
+            
+            let NodeList = document.createElement(arg);
+            // NodeList = Array.from(NodeList);
+            domNode = new DOMNodeCollection([NodeList]);
         }
         debugger;
         this.NodeList.forEach( function(inner){
@@ -39,6 +40,36 @@ class DOMNodeCollection {
         });
 
     }
+
+    attr(attributeName, value) {
+        let attr;
+        this.NodeList.forEach( node => {
+            if(node.hasAttribute(attributeName)) {
+                if(value) {
+                    node.setAttribute(attributeName, value);
+                } else {
+                    if(!attr) {attr = node.getAttribute(attributeName);}
+                }
+            }
+        })
+        if(attr) return attr;
+    }
+
+    addClass(arg) {
+        this.NodeList.forEach(node => {
+            node.className += " " + arg;
+        })
+    }
+
+    removeClass(arg) {
+        this.NodeList.forEach(node => {
+            if(node.className !== null && node.className.includes(arg)) {
+                node.className = node.className.split(' ').filter(el => el !== arg).join(' ');
+            }
+        })
+    }
+
+
 }
 
 
